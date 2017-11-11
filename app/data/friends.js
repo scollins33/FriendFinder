9ffb0f6e517fcb079081251d99200a550207cfed
scollins33 <sean_collins@gmx.com>
2017-11-10 21:05:15 -0500

    functionality complete

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,34 @@
 let friendArray = [
     {
-        name: 'sean',
-        photo: 'www.google.com',
+        name: 'Kermit the Frog',
+        photo: 'https://lumiere-a.akamaihd.net/v1/images/character_themuppets_kermit_09279e8e.jpeg',
+        scores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        name: 'Gonzo',
+        photo: 'https://lumiere-a.akamaihd.net/v1/images/character_themuppets_gonzo_9c3596c6.jpeg?region=0,0,300,300',
         scores: [
             1,
-            2,
-            3,
-            4,
-            5,
             1,
-            2,
-            3,
-            4,
-            5
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
         ]
     }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,8 @@
 // Primary Requires
 const express = require('express');
-let friends = require('../data/friends');
+const path = require('path');
+
+let friends = require(path.join(__dirname, '../data/friends'));
 
 let apiRouter = express.Router();
 
@@ -22,9 +24,51 @@ apiRouter.get('/friends', (req, res) => {
 // POST: /api/friends
 // receives survey results and processes them
 apiRouter.post('/friends', (req, res) => {
-    let submitted = req.body;
-    console.log(submitted);
+
+    // convert the scores to integers
+    let intScores = [];
+    req.body.scores.forEach((each) => {
+        intScores.push(parseInt(each));
+    });
+
+    // create proper JSON
+    let reqJSON = {
+        name: req.body.name,
+        photo: req.body.photo,
+        scores: intScores
+    };
+
+    // we're basically that guy from Myspace... Tom or whatever
+    // set the lowest to the first in the array
+    // lowest will get replaced if the comparison score of the next friend is lower
+    let lowScore = 100;
+    let lowFriend = friends[0];
+
+    // loop through each user and compare to the new user
+    friends.forEach((each) => {
+        let thisScore = compareUsers(reqJSON.scores, each.scores);
+
+        if (thisScore < lowScore) {
+            lowScore = thisScore;
+            lowFriend = each;
+        }
+    });
+
+    friends.push(reqJSON);
+    res.send(lowFriend);
 });
 
+// function to compare scores for users
+// takes New User Score and Stores User Score
+// returns integer score
+function compareUsers(pNew, pStored) {
+    let compScore = 0;
+
+    for (let i = 0; i < pNew.length; i++) {
+        compScore += Math.abs(pNew[i] - pStored[i]);
+    }
+
+    return compScore;
+}
 
 module.exports = apiRouter;
